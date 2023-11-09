7af77e01b992778c24ac181ba88167bee383fc1b
okayu <moka1988jp@gmail.com>
2023-11-09 19:05:08 +0900

    update

--- keyboards/ergock/config.h
+++ keyboards/ergock/config.h
@@ -98,7 +98,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 10
+#define DEBOUNCE 32
+#define DEBOUNCE_TYPE sym_eager_pk
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
--- keyboards/ergock/keymaps/default/keymap.c
+++ keyboards/ergock/keymaps/default/keymap.c
@@ -16,10 +16,13 @@
 #include QMK_KEYBOARD_H
 
 // Defines the keycodes used by our macros in process_record_user
-// enum custom_keycodes {
-//   QMKBEST = SAFE_RANGE,
-//   QMKURL
-// };
+enum custom_keycodes {
+  // QMKBEST = SAFE_RANGE,
+  // QMKURL
+  IMETOGGLE
+};
+
+#define KC_IMETOGGLE IMETOGGLE
 
 #define WIN 0
 #define MAC 1
@@ -43,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , MO(NUM),     MO(NUM), KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC, \
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_LPRN,     KC_RPRN, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT , \
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LBRC,     KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_QUOT, \
-    KC_LCTL, KC_LALT, MO(ADJUST), KC_LGUI, MO(LOWER), KC_SPC , KC_ESC ,     LCTL(KC_SPC), KC_SPC , MO(RAISE), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+    KC_LCTL, KC_LALT, MO(ADJUST), KC_LGUI, MO(LOWER), KC_SPC , KC_ESC ,     KC_IMETOGGLE, KC_SPC , MO(RAISE), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
   ),
   /* Base for Mac
    * ,------------------------------------------------.   ,------------------------------------------------.
@@ -60,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , MO(NUM),     MO(NUM), KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC, \
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_LPRN,     KC_RPRN, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT , \
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LBRC,     KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_QUOT, \
-    KC_LGUI, KC_LALT, MO(ADJUST), KC_LGUI, MO(LOWER), KC_SPC , KC_ESC ,     LCTL(KC_SPC), KC_SPC , MO(RAISE), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+    KC_LGUI, KC_LALT, MO(ADJUST), KC_LGUI, MO(LOWER), KC_SPC , KC_ESC ,     KC_IMETOGGLE, KC_SPC , MO(RAISE), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
   ),
   /* Lower
    * ,------------------------------------------------.   ,------------------------------------------------.
@@ -164,6 +167,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //   }
 //   return true;
 // }
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case IMETOGGLE:
+      if (record->event.pressed) {
+        register_code(KC_LCTRL);
+        register_code(KC_SPACE);
+      } else {
+        unregister_code(KC_SPACE);
+        unregister_code(KC_LCTRL);
+      }
+      break;
+  }
+  return true;
+}
 
 // void matrix_init_user(void) {
 
